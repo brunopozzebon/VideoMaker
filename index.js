e29cfb38453c17439e1ecc548d1e344eed94f32e
Bruno Pozzebon <brunopozzebon44@gmail.com>
2019-07-07 21:44:44 -0300

    Add trends search

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-let readLine = require("readline-sync");
+const readline = require('readline-sync');
+const Parser = require('rss-parser');
 
-function start(){
-    let content = readLine.question("Type de Wikipedia search:");
-    console.log(content);
+const ULR_GOOGLE_TRENDS = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR' ;
+
+async function start () {
+  let content = {};
+  content.searchTerm = await askAndReturnSearchTerm();
+  content.prefix = askAndReturnPrefix();
+
+  async function askAndReturnSearchTerm () {
+    const response = readline.question('Type a Wikipedia search term or G to fetch google trends: ');
+    return (response.toUpperCase() === 'G') ?  await askAndReturnTrend() : response;
+  }
+
+  async function askAndReturnTrend() {
+    const trends = await getGoogleTrends();
+    const choice = readline.keyInSelect(trends, 'Choose your trend:');    
+    return trends[choice];
+  }
+
+  async function getGoogleTrends () {
+    const parser = new Parser();
+    const trends = await parser.parseURL(ULR_GOOGLE_TRENDS);
+    return trends.items.map(i => i.title);
+  }
+
+  function askAndReturnPrefix () {
+    const prefixes = ['Who is', 'What is', 'The history of'];
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ');
+    const selectedPrefixText = prefixes[selectedPrefixIndex];
+    return selectedPrefixText;
+  }
 }
 
 start();
